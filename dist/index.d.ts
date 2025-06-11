7e00ffb2a6742fde6a8d80d6ce2ccbe05d975e09
alexandrtovmach <alexandrtovmach@gmail.com>
2025-06-11 21:44:38 +0200

    feat: Added exports

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,3 +1,5 @@
 import DuckTableSkeleton from './components/duck-table-skeleton';
 import PagePanelSkeleton from './components/page-panel-skeleton';
-export { DuckTableSkeleton, PagePanelSkeleton };
+import DuckLoader from './components/duck-loader';
+import DuckTable from './components/duck-table';
+export { DuckTableSkeleton, PagePanelSkeleton, DuckLoader, DuckTable };
--- index.ts
+++ index.ts
@@ -1,4 +1,6 @@
 import DuckTableSkeleton from './components/duck-table-skeleton';
 import PagePanelSkeleton from './components/page-panel-skeleton';
+import DuckLoader from './components/duck-loader';
+import DuckTable from './components/duck-table';
 
-export { DuckTableSkeleton, PagePanelSkeleton };
+export { DuckTableSkeleton, PagePanelSkeleton, DuckLoader, DuckTable };
