3f8f9402264462d6bf2c09de0b7cb9f6e69e6cee
alexandrtovmach <alexandrtovmach@gmail.com>
2025-07-06 14:42:42 +0200

    fix: Make a build

--- dist/index.js
+++ dist/index.js
@@ -2,4 +2,5 @@ import DuckTableSkeleton from './components/duck-table-skeleton';
 import PagePanelSkeleton from './components/page-panel-skeleton';
 import DuckLoader from './components/duck-loader';
 import DuckTable from './components/duck-table';
+export * from './lib/parse';
 export { DuckTableSkeleton, PagePanelSkeleton, DuckLoader, DuckTable };
